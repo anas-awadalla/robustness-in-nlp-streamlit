da832062d5f6cc1e22570a50b6f08b0d69dac105
Anas Awadalla <anasa2@uw.edu>
2022-03-08 02:43:41 -0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -10,7 +10,7 @@ from sklearn.utils import resample
 from transformers.data.metrics.squad_metrics import compute_f1
 import os
 import plotly.graph_objects as go
-
+import matplotlib.pyplot as plt
 
 """
 The following datasets are available along with thier sizes:
