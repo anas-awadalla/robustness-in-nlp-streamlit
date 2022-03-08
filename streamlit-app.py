535151f8738311b422bf302cbe9c2303e51ecff7
Anas Awadalla <anasa2@uw.edu>
2022-03-08 02:40:04 -0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -158,66 +158,176 @@ if hide_few_shot:
     
 if hide_icl:
     dataset_df = dataset_df[dataset_df['type'] != 'icl']
+
+if not logit_scaling:
     
-fig = px.scatter(dataset_df, x="iid_f1", y="ood_f1", color="model_family",
-                 hover_data=["model_name", "type"], error_x="e_plus_iid", error_x_minus="e_minus_iid",
-                 error_y="e_plus_ood", error_y_minus="e_minus_ood", title=f"Performance Comparison Between {id_dataset} and {dataset}",
-                 labels=dict(iid_f1=f"F1 Score Performance on {id_dataset}", ood_f1=f"F1 Score Performance on {dataset}"))
+    fig = px.scatter(dataset_df, x="iid_f1", y="ood_f1", color="model_family",
+                     hover_data=["model_name", "type"], error_x="e_plus_iid", error_x_minus="e_minus_iid",
+                     error_y="e_plus_ood", error_y_minus="e_minus_ood", title=f"Performance Comparison Between {id_dataset} and {dataset}",
+                     labels=dict(iid_f1=f"F1 Score Performance on {id_dataset}", ood_f1=f"F1 Score Performance on {dataset}"))
 
-if not hide_finetuned:
-    # Add trendline for finetuned models
-    z = np.polyfit(dataset_df[dataset_df['type'] == 'finetuned']['iid_f1'],
-                   dataset_df[dataset_df['type'] == 'finetuned']['ood_f1'], 1)
-    y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'finetuned']['iid_f1'])
+    if not hide_finetuned:
+        # Add trendline for finetuned models
+        z = np.polyfit(dataset_df[dataset_df['type'] == 'finetuned']['iid_f1'],
+                       dataset_df[dataset_df['type'] == 'finetuned']['ood_f1'], 1)
+        y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'finetuned']['iid_f1'])
 
-    line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'finetuned']['ood_f1'] ,y_fit):0.3f}"
-    fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'finetuned']
-                   ['iid_f1'], y=y_fit, name='Fine-Tuned Fit:'+line_equation, mode='lines'))
+        line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'finetuned']['ood_f1'] ,y_fit):0.3f}"
+        fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'finetuned']
+                       ['iid_f1'], y=y_fit, name='Fine-Tuned Fit:'+line_equation, mode='lines'))
 
-if not hide_zero_shot:
-    # Add trendline for zero-shot models
-    z = np.polyfit(dataset_df[dataset_df['type'] == 'zeroshot']['iid_f1'],
-                   dataset_df[dataset_df['type'] == 'zeroshot']['ood_f1'], 1)
-    y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'zeroshot']['iid_f1'])
+    if not hide_zero_shot:
+        # Add trendline for zero-shot models
+        z = np.polyfit(dataset_df[dataset_df['type'] == 'zeroshot']['iid_f1'],
+                       dataset_df[dataset_df['type'] == 'zeroshot']['ood_f1'], 1)
+        y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'zeroshot']['iid_f1'])
 
-    line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'zeroshot']['ood_f1'] ,y_fit):0.3f}"
-    fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'zeroshot']
-                   ['iid_f1'], y=y_fit, name='Zero-Shot Fit:'+line_equation, mode='lines'))
+        line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'zeroshot']['ood_f1'] ,y_fit):0.3f}"
+        fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'zeroshot']
+                       ['iid_f1'], y=y_fit, name='Zero-Shot Fit:'+line_equation, mode='lines'))
 
-if not hide_few_shot:
-    # Add trendline for few-shot models
-    z = np.polyfit(dataset_df[dataset_df['type'] == 'fewshot']['iid_f1'],
-                   dataset_df[dataset_df['type'] == 'fewshot']['ood_f1'], 1)
-    y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'fewshot']['iid_f1'])
+    if not hide_few_shot:
+        # Add trendline for few-shot models
+        z = np.polyfit(dataset_df[dataset_df['type'] == 'fewshot']['iid_f1'],
+                       dataset_df[dataset_df['type'] == 'fewshot']['ood_f1'], 1)
+        y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'fewshot']['iid_f1'])
 
-    line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'fewshot']['ood_f1'] ,y_fit):0.3f}"
-    fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'fewshot']
-                   ['iid_f1'], y=y_fit, name='Few-Shot Fit:'+line_equation, mode='lines'))
+        line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'fewshot']['ood_f1'] ,y_fit):0.3f}"
+        fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'fewshot']
+                       ['iid_f1'], y=y_fit, name='Few-Shot Fit:'+line_equation, mode='lines'))
 
-if not hide_icl:
-    # Add trendline for icl models
-    z = np.polyfit(dataset_df[dataset_df['type'] == 'icl']['iid_f1'],
-                   dataset_df[dataset_df['type'] == 'icl']['ood_f1'], 1)
-    y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'icl']['iid_f1'])
+    if not hide_icl:
+        # Add trendline for icl models
+        z = np.polyfit(dataset_df[dataset_df['type'] == 'icl']['iid_f1'],
+                       dataset_df[dataset_df['type'] == 'icl']['ood_f1'], 1)
+        y_fit = np.poly1d(z)(dataset_df[dataset_df['type'] == 'icl']['iid_f1'])
 
-    line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'icl']['ood_f1'] ,y_fit):0.3f}"
-    fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'icl']
-                   ['iid_f1'], y=y_fit, name='ICL Fit:'+line_equation, mode='lines'))
+        line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df[dataset_df['type'] == 'icl']['ood_f1'] ,y_fit):0.3f}"
+        fig.add_traces(go.Scatter(x=dataset_df[dataset_df['type'] == 'icl']
+                       ['iid_f1'], y=y_fit, name='ICL Fit:'+line_equation, mode='lines'))
 
-fig.add_shape(type='line',
-                x0=0,
-                y0=0,
-                x1=100,
-                y1=100,
-                line=dict(color='Blue',),
-                line_dash="dash",
-                xref='x',
-                yref='y')
+    fig.add_shape(type='line',
+                    x0=0,
+                    y0=0,
+                    x1=100,
+                    y1=100,
+                    line=dict(color='Blue',),
+                    line_dash="dash",
+                    xref='x',
+                    yref='y')
+else:
+        fig, ax = plt.subplots()
+        
+        # From: https://github.com/mlfoundations/wise-ft/blob/master/src/scatter_plot.py
+        def adjust_plot(ax, xrange, yrange, scaling, tick_freq=0.05):
+            if type(tick_freq) is tuple:
+                xtick_freq, ytick_freq = tick_freq[0], tick_freq[1]
+            else:
+                xtick_freq, ytick_freq = tick_freq, tick_freq
 
-if logit_scaling:
-    fig.update_xaxes(title_text=f"F1 Score Performance on {id_dataset} (Logit Scaling)", type="log", range=[0,2])
-    fig.update_yaxes(title_text=f"F1 Score Performance on {dataset} (Logit Scaling)", type="log", range=[0,2])
+            if scaling == 'probit':
+                h = scipy.stats.norm.ppf
+            elif scaling == 'logit':
+                h = lambda p: np.log(p / (1 - p))
+            else:
+                h = lambda p: p
 
+            def transform(z):
+                return [h(p) for p in z]
+
+            tick_loc_x = [round(z, 2) for z in np.arange(xrange[0], xrange[1], xtick_freq)]
+            ax.set_xticks(transform(tick_loc_x))
+            ax.set_xticklabels([str(round(loc * 100)) for loc in tick_loc_x], fontsize=13)
+
+            tick_loc_y = [round(z, 2) for z in np.arange(yrange[0], yrange[1], ytick_freq)]
+            ax.set_yticks(transform(tick_loc_y))
+            ax.set_yticklabels([str(round(loc * 100)) for loc in tick_loc_y], fontsize=13)
+
+            ax.set_ylim(h(yrange[0]), h(yrange[1]))
+            ax.set_xlim(h(xrange[0]), h(xrange[1]))
+
+            ax.grid(linewidth=0.5)
+
+            return transform
+        
+        y_range = (0.95 * min(dataset_df['ood_f1']), 1.05 * max(dataset_df['ood_f1']))
+        x_range = (0.95 * min(dataset_df['iid_f1']), 1.05 * max(dataset_df['iid_f1']))
+        
+        transform = adjust_plot(ax, x_range, y_range, scaling='logit')
+        
+        zeroshot_xs = dataset_df[dataset_df["type"] == "zeroshot"]["iid_f1"]
+        zeroshot_ys = dataset_df[dataset_df["type"] == "zeroshot"]["ood_f1"]
+        
+        # Add zero-shot
+        ax.scatter(transform(zeroshot_xs), transform(zeroshot_ys),
+            label=f"Zeroshot Models", marker='*', s=200, alpha=0.8, c='C4',
+        )
+        
+        ### Zero-shot linear fit
+        sorted_pts = sorted(zip(zeroshot_xs, zeroshot_ys), key=lambda x: x[0])
+        zeroshot_xs, zeroshot_ys = zip(*sorted_pts)
+        z_zeroshot = np.polyfit(transform(zeroshot_xs), transform(zeroshot_ys), 1)
+        y_linear_zeroshot = np.poly1d(z_zeroshot)(transform(zeroshot_xs))
+
+        ax.plot(transform(zeroshot_xs), y_linear_zeroshot, "y-", label="Zeroshot Fit"+ f" $y={z_zeroshot[0]:0.3f}\;x{z_zeroshot[1]:+0.3f}$\n$R^2 = {r2_score(transform(zeroshot_ys),y_linear_zeroshot):0.3f}$")
+
+        finetuned_xs = dataset_df[dataset_df["type"] == "finetuned"]["iid_f1"]
+        finetuned_ys = dataset_df[dataset_df["type"] == "finetuned"]["ood_f1"]
+        
+        # Add fine-tuned
+        ax.scatter(transform(finetuned_xs), transform(finetuned_ys),
+            label=f"Finetuned Models", marker='D', s=100, alpha=0.8, c='C1',
+        )
+            
+        ### Fine-tuned linear fit
+        sorted_pts = sorted(zip(finetuned_xs, finetuned_ys), key=lambda x: x[0])
+        finetuned_xs, finetuned_ys = zip(*sorted_pts)
+        z_finetuned = np.polyfit(transform(finetuned_xs), transform(finetuned_ys), 1)
+        y_linear_finetuned = np.poly1d(z_finetuned)(transform(finetuned_xs))
+
+        ax.plot(transform(finetuned_xs), y_linear_finetuned, "r-", label="Finetuned Fit"+ f" $y={z_finetuned[0]:0.3f}\;x{z_finetuned[1]:+0.3f}$\n$R^2 = {r2_score(transform(finetuned_ys),y_linear_finetuned):0.3f}$")
+        
+        finetuned_xs = dataset_df[dataset_df["type"] == "icl"]["iid_f1"]
+        finetuned_ys = dataset_df[dataset_df["type"] == "icl"]["ood_f1"]
+        
+        # Add ICL
+        ax.scatter(transform(finetuned_xs), transform(finetuned_ys),
+            label=f"Finetuned Models", marker='D', s=100, alpha=0.8, c='C1',
+        )
+            
+        ### Fine-tuned linear fit
+        sorted_pts = sorted(zip(finetuned_xs, finetuned_ys), key=lambda x: x[0])
+        finetuned_xs, finetuned_ys = zip(*sorted_pts)
+        z_finetuned = np.polyfit(transform(finetuned_xs), transform(finetuned_ys), 1)
+        y_linear_finetuned = np.poly1d(z_finetuned)(transform(finetuned_xs))
+
+        ax.plot(transform(finetuned_xs), y_linear_finetuned, "r-", label="ICL Fit"+ f" $y={z_finetuned[0]:0.3f}\;x{z_finetuned[1]:+0.3f}$\n$R^2 = {r2_score(transform(finetuned_ys),y_linear_finetuned):0.3f}$")
+        
+        finetuned_xs = dataset_df[dataset_df["type"] == "fewshot"]["iid_f1"]
+        finetuned_ys = dataset_df[dataset_df["type"] == "fewshot"]["ood_f1"]
+        
+        # Add Fewshot
+        ax.scatter(transform(finetuned_xs), transform(finetuned_ys),
+            label=f"Finetuned Models", marker='.', s=100, alpha=0.8, c='C1',
+        )
+            
+        ### Fewshot linear fit
+        sorted_pts = sorted(zip(finetuned_xs, finetuned_ys), key=lambda x: x[0])
+        finetuned_xs, finetuned_ys = zip(*sorted_pts)
+        z_finetuned = np.polyfit(transform(finetuned_xs), transform(finetuned_ys), 1)
+        y_linear_finetuned = np.poly1d(z_finetuned)(transform(finetuned_xs))
+
+        ax.plot(transform(finetuned_xs), y_linear_finetuned, "r-", label="Fewshot Fit"+ f" $y={z_finetuned[0]:0.3f}\;x{z_finetuned[1]:+0.3f}$\n$R^2 = {r2_score(transform(finetuned_ys),y_linear_finetuned):0.3f}$")
+        
+        ### y=x line
+        ax.plot(min([ax.get_xlim(), ax.get_ylim()]), max([ax.get_xlim(), ax.get_ylim()]), '--', label="y=x")
+        
+        ax.legend(loc='best', prop={'size': 8})
+
+        plt.title(f"Performance on {dataset} vs {id_dataset}")
+        plt.xlabel(f"F1 Scores on {id_dataset}")
+        plt.ylabel(f"F1 Scores on {dataset}")
+        
 dataset_df = dataset_df.rename(columns={"iid_f1": "id_f1"})
 dataset_df = dataset_df.drop(columns=["iid_bootstrap_f1", "ood_bootstrap_f1"])
 dataset_df = dataset_df.rename(columns={"e_minus_iid": "e_minus_id"})
