96c1152a154bebbcb0d2549c2285e8184f2fca68
Anas Awadalla <anasa2@uw.edu>
2022-05-24 13:52:54 -0700

    added aggregation

---
+++ annotate.py
@@ -0,0 +1,37 @@
+# A script that takes in a csv and prompts user for each unique model name to fill in the following properties
+# 1. is_adapter
+# 2. model_size in millions
+# 3. is_robust_model
+
+import pandas as pd
+from tqdm import tqdm
+import os
+
+data = pd.read_csv('results/extractive_question_answering.csv')
+annotated_data = pd.read_csv('results/annotated_data.csv') if os.path.exists('results/annotated_data.csv') else pd.DataFrame(columns=['model_name', 'is_adapter', 'model_size', 'is_robust'])
+
+# Get unique model names minus the ones that are already annotated
+models = set(data['model_name'].unique().tolist()).difference(set(annotated_data['model_name'].unique().tolist()))
+
+# order the models
+models = sorted(models)
+
+# go through each unique model name
+for model_name in tqdm(models):
+
+    # prompt user for each of the following properties
+    is_adapter = input('Is ' + model_name + ' an adapter? (y/n): ')
+    if is_adapter == "exit":
+        break
+    is_adapter = is_adapter == "y"
+    is_robust = input('Is ' + model_name + ' robust? (y/n): ') == 'y'
+
+    model_size = None
+    while not model_size:
+        model_size = int(input('What is the model size of ' + model_name + ' in millions? '))
+
+    # append the new row to the annotated data
+    annotated_data = annotated_data.append({'model_name': model_name, 'is_adapter': is_adapter, 'model_size': model_size, 'is_robust': is_robust}, ignore_index=True)
+
+# save the annotated data
+annotated_data.to_csv('results/annotated_data.csv', index=False)
--- streamlit-app.py
+++ streamlit-app.py
@@ -10,24 +10,31 @@ import os
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 import scipy
+import random
 
-"""
-The following datasets are available along with thier sizes:
-'squad': 10570,
-'squadshifts_nyt': 10065,
-'squadshifts_reddit': 9803,
-'squadshifts_new_wiki': 7938,
-'squadshifts_amazon': 9885,
-'RACE': 674, 
-'DROP': 1503, 
-'BioASQ': 1504,
-'RelationExtraction': 2948,
-'NewsQA': 4212,
-'TriviaQA-web': 7785,
-'SearchQA': 16980,
-'HotpotQA': 5904,
-'NaturalQuestionsShort': 12836
-"""
+st.set_page_config(
+    page_title="QA Robustness",
+    layout="wide",
+)
+
+# """
+# The following datasets are available along with thier sizes:
+# 'squad': 10570,
+# 'squadshifts_nyt': 10065,
+# 'squadshifts_reddit': 9803,
+# 'squadshifts_new_wiki': 7938,
+# 'squadshifts_amazon': 9885,
+# 'RACE': 674, 
+# 'DROP': 1503, 
+# 'BioASQ': 1504,
+# 'RelationExtraction': 2948,
+# 'NewsQA': 4212,
+# 'TriviaQA-web': 7785,
+# 'SearchQA': 16980,
+# 'HotpotQA': 5904,
+# 'NaturalQuestionsShort': 12836
+# """
+    
 
 dataset_to_size = {
     'squad': 10570,
@@ -48,9 +55,11 @@ dataset_to_size = {
     'NaturalQuestionsShort': 12836
 }
 
+average_all_datasets = st.sidebar.checkbox("Average All Datasets", value=False)
+
 dataset = st.sidebar.selectbox(
     'Dataset (Y-Axis)',
-    list(dataset_to_size.keys()))
+    list(dataset_to_size.keys()), disabled=average_all_datasets)
 
 id_dataset = st.sidebar.selectbox(
     'Dataset (X-Axis)',
@@ -69,16 +78,65 @@ pandas_id_dataset = id_dataset
 scaling = st.sidebar.selectbox("Graph Scaling", ['Linear', 'Logit', 'Probit'])
 color_by_dataset = st.sidebar.checkbox("Color by Pretraining Dataset", value=False)
 
+results_path = Path(".") / "results"
+
+df = pd.read_csv(os.path.join(results_path.absolute(),
+                 'extractive_question_answering.csv'))
+df_annotated = pd.read_csv(os.path.join(results_path.absolute(),"annotated_data.csv"))
+
+# join df and df_annotated by model_name
+df = df.merge(df_annotated, on='model_name')
+df = df.assign(alpha=1.0)
+
+original_df = df.copy()
+
+filter_min_size=None
+filter_max_size=None
+filter_model_types=None
+filter_finetune_types=None
+filter_enhance_types=None
+
+st.markdown("""Run a Query""")
+filter_model_types = st.multiselect("Model Family", df["model_family"].unique().tolist())
+filter_finetune_types = st.multiselect("Finetune Type", df["type"].unique().tolist())
+filter_enhance_types = st.multiselect("Enhance Type", ["Adapters", "Robustness"])
+# filter_min_size = st.number_input("Minimum Model Size (in millions of parameters)", value=0, step=10, min_value=0, max_value=1500)
+# filter_max_size = st.number_input("Maximum Model Size (in millions of parameters)", value=1500, step=10, min_value=0, max_value=1500)
+
+# For models not in the filter_model_types or filter_finetune_types list, set their alpha to 0.2
+# if filter_model_types:
+#     df.loc[~df['model_family'].isin(filter_model_types), 'alpha'] = 0.2
+
+# if filter_finetune_types:
+#     df.loc[~df['type'].isin(filter_finetune_types), 'alpha'] = 0.2
+
+# if adapter in filter_enhance_types set alpha to 0.2 for all models that is_adapter is false
+# if "Adapters" in filter_enhance_types:
+#     df.loc[df['is_adapter'] == False, 'alpha'] = 0.2
+
+# # same for is_robust
+# if "Robustness" in filter_enhance_types:
+#     df.loc[df['is_robust'] == False, 'alpha'] = 0.2
+
+
+# # set alpha to 0.2 for models whose size is less than filter_min_size
+# if filter_min_size is not None:
+#     df.loc[df['model_size'] < filter_min_size, 'alpha'] = 0.2
+
+# # set alpha to 0.2 for models whose size is greater than filter_max_size
+# if filter_max_size is not None:
+#     df.loc[df['model_size'] > filter_max_size, 'alpha'] = 0.2
+
+
 hide_zero_shot = st.sidebar.checkbox("Hide Zero Shot Models", value=False)
 hide_few_shot = st.sidebar.checkbox("Hide Few Shot Models", value=False)
 hide_icl = st.sidebar.checkbox("Hide In Context Learning Models", value=False)
 hide_finetuned = st.sidebar.checkbox("Hide Finetuned Models", value=False)
 
+df['f1'] = pd.to_numeric(df["f1"])
+df['f1_lower'] = pd.to_numeric(df["f1_lower"])
+df['f1_upper'] = pd.to_numeric(df["f1_upper"])
 
-results_path = Path(".") / "results"
-
-df = pd.read_csv(os.path.join(results_path.absolute(),
-                 'extractive_question_answering.csv'))
 df.drop_duplicates(inplace=True)
 
 df['dataset'] = df['dataset'].str.lower()
@@ -89,10 +147,21 @@ hidden_models = set(df["model_family"].unique()).difference(visible_models)
 for hidden in hidden_models:
     df = df[df['model_family'] != hidden]
 
-ood_df = df.loc[df['dataset'] == pandas_dataset.lower()].drop(columns=['dataset'])
+if average_all_datasets:
+    ood_df = df[df['dataset'] != pandas_id_dataset.lower()]
+
+    # average all f1 scores for datasets per model
+    ood_df = ood_df.groupby(['model_name'])['f1', 'f1_lower', 'f1_upper'].mean()
+    # zero out f1_lower and f1_upper
+    ood_df['f1_lower'] = 0
+    ood_df['f1_upper'] = 0
+    ood_df = ood_df.reset_index()
+else:
+    ood_df = df.loc[df['dataset'] == pandas_dataset.lower()].drop(columns=['dataset'])
+    
 iid_df = df.loc[df['dataset'] == pandas_id_dataset.lower()].drop(columns=['dataset'])
 
-ood_df = ood_df.drop(columns=['type', 'model_family'])
+ood_df = ood_df.drop(columns=['type', 'model_family', 'model_size', 'is_robust', 'is_adapter', 'alpha'], errors='ignore')
 iid_df = iid_df.rename(columns={"f1": "iid_f1"})
 iid_df = iid_df.rename(columns={"f1_lower": "iid_f1_lower"})
 iid_df = iid_df.rename(columns={"f1_upper": "iid_f1_upper"})
@@ -148,24 +217,19 @@ dataset_map = {"bert": "bookcorpus+wikipedia", "bart": "bookcorpus+wikipedia", "
 dataset_df['pretrain_dataset'] = dataset_df['model_family'].apply(lambda x: dataset_map[x])
 
 hover_data = {"iid_f1": True, "ood_f1": True, "type": True, "model_family": True, "pretrain_dataset": True, "iid_f1_transformed": False, "ood_f1_transformed": False}
-
 if scaling == 'Linear':
-    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name",  hover_data=hover_data, error_x="iid_f1_upper", error_x_minus="iid_f1_lower", error_y="ood_f1_upper", error_y_minus="ood_f1_lower", title=f"Performance Comparison Between {pandas_id_dataset} and {pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {pandas_dataset}"))
-
+    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name", hover_data=hover_data, error_x="iid_f1_upper", error_x_minus="iid_f1_lower", error_y="ood_f1_upper", error_y_minus="ood_f1_lower", title=f"Performance Comparison Between {pandas_id_dataset} and {'All Datasets' if average_all_datasets else pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {'All Datasets' if average_all_datasets else pandas_dataset}"), opacity=0.5)
 else:
-    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name", hover_data=hover_data, title=f"Performance Comparison Between {pandas_id_dataset} and {pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {pandas_dataset}"))
+    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name", hover_data=hover_data, title=f"Performance Comparison Between {pandas_id_dataset} and {'All Datasets' if average_all_datasets else pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {'All Datasets' if average_all_datasets else pandas_dataset}"), opacity=0.5)
 
 
 tick_loc_x = [round(z) for z in np.arange(xrange[0], xrange[1], 5)]
-print(tick_loc_x)
-print(transform(tick_loc_x, scaling=scaling))
 
 # set x axis ticks
 fig.update_xaxes(tickmode='array', tickvals=transform(tick_loc_x, scaling=scaling), ticktext=[str(z) for z in tick_loc_x])
 
 tick_loc_y = [round(z) for z in np.arange(yrange[0], yrange[1], 5)]
-print(tick_loc_y)
-print(transform(tick_loc_y, scaling=scaling))
+
 # set y axis ticks
 fig.update_yaxes(tickmode='array', tickvals=transform(tick_loc_y, scaling=scaling), ticktext=[str(z) for z in tick_loc_y])
 
@@ -219,6 +283,32 @@ if not hide_icl:
         line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(icl_df['ood_f1_transformed'] ,y_fit):0.3f}"
         fig.add_traces(go.Scatter(x=icl_df
                         ['iid_f1_transformed'], y=y_fit, name='ICL Fit:'+line_equation, mode='lines'))
+
+if filter_model_types or filter_finetune_types or filter_enhance_types:
+    query_df = dataset_df.copy()
+    if filter_model_types:
+        query_df = dataset_df[dataset_df["model_family"].isin(filter_model_types)]
+    
+    if filter_finetune_types:
+        query_df = query_df[query_df["type"].isin(filter_finetune_types)]
+    
+    if "Adapters" in filter_enhance_types:
+        query_df = query_df.loc[query_df['is_adapter'] == True]
+
+    # same for is_robust
+    if "Robustness" in filter_enhance_types:
+        query_df = query_df.loc[query_df['is_robust'] == True]
+        
+    if len(query_df) != 0:
+        # Add trendline for icl models
+        z = np.polyfit(query_df['iid_f1_transformed'],
+                        query_df['ood_f1_transformed'], 1)
+        y_fit = np.poly1d(z)(query_df['iid_f1_transformed'])
+
+        line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(query_df['ood_f1_transformed'] ,y_fit):0.3f}"
+        fig.add_traces(go.Scatter(x=query_df
+                        ['iid_f1_transformed'], y=y_fit, name='Query Fit:'+line_equation, mode='lines'))
+    
 
 # Plot y=x line using tick values
 fig.add_trace(go.Line(x=transform(tick_loc_y), y=transform(tick_loc_y), mode='lines', name='y=x', line_dash="dash", line_color="blue",hoverinfo='none'))
