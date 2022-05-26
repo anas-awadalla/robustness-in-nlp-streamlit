db1fa545895f83734e262d859ca4cc9f407bae9e
Anas Awadalla <anasa2@uw.edu>
2022-05-25 23:33:41 -0700

    added more features

--- streamlit-app.py
+++ streamlit-app.py
@@ -100,6 +100,11 @@ st.markdown("""Run a Query""")
 filter_model_types = st.multiselect("Model Family", df["model_family"].unique().tolist())
 filter_finetune_types = st.multiselect("Finetune Type", df["type"].unique().tolist())
 filter_enhance_types = st.multiselect("Enhance Type", ["Adapters", "Robustness"])
+
+is_filtered = filter_model_types or filter_finetune_types or filter_enhance_types
+
+# Add a textbox for query name
+query_name = st.text_input("Query Name (for labeling purposing)", "")
 # filter_min_size = st.number_input("Minimum Model Size (in millions of parameters)", value=0, step=10, min_value=0, max_value=1500)
 # filter_max_size = st.number_input("Maximum Model Size (in millions of parameters)", value=1500, step=10, min_value=0, max_value=1500)
 
@@ -211,16 +216,17 @@ else:
     dataset_df.drop(columns=['iid_f1_lower', 'iid_f1_upper', 'ood_f1_lower', 'ood_f1_upper'], inplace=True)
 
 # Create a dictionary of model_family: pretraining dataset
-dataset_map = {"bert": "bookcorpus+wikipedia", "bart": "bookcorpus+wikipedia", "minilm": "bookcorpus+wikipedia",  "albert": "bookcorpus+wikipedia", "roberta": "bookcorpus+wikipedia+cc-news+openwebtext+stories", "gpt": "webtext", "bidaf":"none (Word2Vec or ELMO)", "spanbert": "bookcorpus+wikipedia", "t5": "C4", "adapter-roberta": "bookcorpus+wikipedia+cc-news+openwebtext+stories", "adapter-bert": "bookcorpus+wikipedia", "xlm-roberta": "bookcorpus+wikipedia+cc-news+openwebtext+stories", "gpt-neo": "pile", "splinter": "wikipedia+bookcorpus", "opt":"wikipedia+bookcorpus"}
+dataset_map = {"bert": "bookcorpus+wikipedia", "bart": "bookcorpus+wikipedia", "minilm": "bookcorpus+wikipedia",  "albert": "bookcorpus+wikipedia", "roberta": "bookcorpus+wikipedia+cc-news+openwebtext+stories", "gpt": "webtext", "bidaf":"none (Word2Vec or ELMO)", "spanbert": "bookcorpus+wikipedia", "t5": "C4", "adapter-roberta": "bookcorpus+wikipedia+cc-news+openwebtext+stories", "adapter-bert": "bookcorpus+wikipedia", "xlm-roberta": "bookcorpus+wikipedia+cc-news+openwebtext+stories", "gpt-neo": "pile", "splinter": "wikipedia+bookcorpus", "opt":"pile"}
 
 # Add a pretrain_dataset column and iterate over the rows to add the pretrain_dataset
 dataset_df['pretrain_dataset'] = dataset_df['model_family'].apply(lambda x: dataset_map[x])
 
 hover_data = {"iid_f1": True, "ood_f1": True, "type": True, "model_family": True, "pretrain_dataset": True, "iid_f1_transformed": False, "ood_f1_transformed": False}
 if scaling == 'Linear':
-    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name", hover_data=hover_data, error_x="iid_f1_upper", error_x_minus="iid_f1_lower", error_y="ood_f1_upper", error_y_minus="ood_f1_lower", title=f"Performance Comparison Between {pandas_id_dataset} and {'All Datasets' if average_all_datasets else pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {'All Datasets' if average_all_datasets else pandas_dataset}"), opacity=0.5)
+    # plot scatter by model family
+    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name", hover_data=hover_data, error_x="iid_f1_upper", error_x_minus="iid_f1_lower", error_y="ood_f1_upper", error_y_minus="ood_f1_lower", title=f"Performance Comparison Between {pandas_id_dataset} and {'All Datasets' if average_all_datasets else pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {'All Datasets' if average_all_datasets else pandas_dataset}"), opacity=0.8 if not is_filtered else 0.1)
 else:
-    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name", hover_data=hover_data, title=f"Performance Comparison Between {pandas_id_dataset} and {'All Datasets' if average_all_datasets else pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {'All Datasets' if average_all_datasets else pandas_dataset}"), opacity=0.5)
+    fig = px.scatter(dataset_df, x="iid_f1_transformed", y="ood_f1_transformed", color="pretrain_dataset" if color_by_dataset else "model_family", hover_name="model_name", hover_data=hover_data, title=f"Performance Comparison Between {pandas_id_dataset} and {'All Datasets' if average_all_datasets else pandas_dataset}", labels=dict(iid_f1_transformed=f"F1 Score Performance on {pandas_id_dataset}", ood_f1_transformed=f"F1 Score Performance on {'All Datasets' if average_all_datasets else pandas_dataset}"), opacity=0.8 if not is_filtered else 0.1)
 
 
 tick_loc_x = [round(z) for z in np.arange(xrange[0], xrange[1], 5)]
@@ -236,7 +242,7 @@ fig.update_yaxes(tickmode='array', tickvals=transform(tick_loc_y, scaling=scalin
 # turn off autoscale
 fig.update_layout(autosize=False)
 
-if not hide_finetuned:
+if not hide_finetuned and not is_filtered:
     finetuned_df = dataset_df[dataset_df["type"] == "finetuned"]
     if len(finetuned_df) != 0:
         # Add trendline for finetuned models
@@ -248,7 +254,7 @@ if not hide_finetuned:
         fig.add_traces(go.Scatter(x=finetuned_df
                         ['iid_f1_transformed'], y=y_fit, name='Fine-Tuned Fit:'+line_equation, mode='lines'))
 
-if not hide_zero_shot:
+if not hide_zero_shot and not is_filtered:
     zeroshot_df = dataset_df[dataset_df["type"] == "zeroshot"]
     if len(zeroshot_df) != 0:
         # Add trendline for zero-shot models
@@ -260,7 +266,7 @@ if not hide_zero_shot:
         fig.add_traces(go.Scatter(x=zeroshot_df
                         ['iid_f1_transformed'], y=y_fit, name='Zero-Shot Fit:'+line_equation, mode='lines'))
 
-if not hide_few_shot:
+if not hide_few_shot and not is_filtered:
     fewshot_df = dataset_df[dataset_df["type"] == "fewshot"]
     if len(fewshot_df) != 0:
         # Add trendline for few-shot models
@@ -272,7 +278,7 @@ if not hide_few_shot:
         fig.add_traces(go.Scatter(x=fewshot_df
                         ['iid_f1_transformed'], y=y_fit, name='Few-Shot Fit:'+line_equation, mode='lines'))
 
-if not hide_icl:
+if not hide_icl and not is_filtered:
     icl_df = dataset_df[dataset_df["type"] == "icl"]
     if len(icl_df) != 0:
         # Add trendline for icl models
@@ -284,7 +290,7 @@ if not hide_icl:
         fig.add_traces(go.Scatter(x=icl_df
                         ['iid_f1_transformed'], y=y_fit, name='ICL Fit:'+line_equation, mode='lines'))
 
-if filter_model_types or filter_finetune_types or filter_enhance_types:
+if is_filtered:
     query_df = dataset_df.copy()
     if filter_model_types:
         query_df = dataset_df[dataset_df["model_family"].isin(filter_model_types)]
@@ -307,7 +313,18 @@ if filter_model_types or filter_finetune_types or filter_enhance_types:
 
         line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(query_df['ood_f1_transformed'] ,y_fit):0.3f}"
         fig.add_traces(go.Scatter(x=query_df
-                        ['iid_f1_transformed'], y=y_fit, name='Query Fit:'+line_equation, mode='lines'))
+                        ['iid_f1_transformed'], y=y_fit, name=f'{query_name if query_name else "Query"} Fit:'+line_equation, mode='lines'))
+    
+    # Plot all other models
+    if len(dataset_df) != 0:
+        # Add trendline for icl models
+        z = np.polyfit(dataset_df['iid_f1_transformed'],
+                        dataset_df['ood_f1_transformed'], 1)
+        y_fit = np.poly1d(z)(dataset_df['iid_f1_transformed'])
+
+        line_equation = f" y={z[0]:0.3f}x{z[1]:+0.3f} -- R^2 = {r2_score(dataset_df['ood_f1_transformed'] ,y_fit):0.3f}"
+        fig.add_traces(go.Scatter(x=dataset_df
+                        ['iid_f1_transformed'], y=y_fit, name='All Models Fit:'+line_equation, mode='lines'))
     
 
 # Plot y=x line using tick values
